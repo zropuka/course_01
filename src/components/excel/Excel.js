a7438eaa1bbf8946e032ff60bbefd7bbb88407ce
learn name <testemail>
2020-07-02 19:35:04 +0300

    small fixes

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,11 +12,11 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      // DEBUG
-      if (component.name) {
-        window['c' + component.name] = component
-      }
-      // DEBUG
+      // // DEBUG
+      // if (component.name) {
+      //   window['c' + component.name] = component
+      // }
+      // // DEBUG
       $el.html(component.toHTML())
       $root.append($el)
       return component
