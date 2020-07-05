b406e045ba4e414aa5ddcb2ea2227fb899ad5e89
learn name <testemail>
2020-07-05 12:58:53 +0300

    Finish base table logic

---
+++ src/core/Emitter.js
@@ -0,0 +1,39 @@
+export class Emitter {
+  constructor() {
+    this.listeners = {}
+  }
+
+  emit(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false
+    }
+    this.listeners[event].forEach(listener => {
+      listener(...args)
+    })
+    return true
+  }
+
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(fn)
+    return () => {
+      this.listeners[event] = this.listeners[event].filter(listener => listener !== fn)
+    }
+  }
+}
+
+// const emitter = new Emitter()
+
+// const unsub = emitter.subscribe('test:event', data => console.log('Sub: ', data))
+
+// setTimeout(() => {
+//   emitter.emit('test:event', 'After 2 seconds')
+// }, 2000)
+
+// setTimeout(() => {
+//   unsub()
+// }, 3000)
+
+// setTimeout(() => {
+//   emitter.emit('test:event', 'After 4 seconds')
+// }, 4000)
--- src/core/ExcelCompoenent.js
+++ src/core/ExcelCompoenent.js
@@ -4,6 +4,15 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscribers = []
+
+    this.prepare()
+  }
+
+  // Setup component before calling init()
+  prepare() {
+    
   }
 
   // Returns a component template
@@ -11,11 +20,27 @@ export class ExcelComponent extends DomListener {
     return ''
   }
 
+  // Notify all linteners about event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Subscribe on event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
+  // Initialize component
+  // Adds listeners
   init() {
     this.initDOMListeners()
   }
 
+  // Destroy component
+  // Removes listeners
   destroy() {
     this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
